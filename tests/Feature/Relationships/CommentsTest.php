bf739a10fe0ae5672756e91faa89205571d6b97c
AntoineC <antoine@a-f.studio>
2020-09-22 00:05:28 +0200

    scaffold comment test

---
+++ tests/Feature/Relationships/CommentsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Relationships;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function a_visitor_can_show_a_comment_with_its_relationships()
+    {
+        // TODO
+    }
+}
