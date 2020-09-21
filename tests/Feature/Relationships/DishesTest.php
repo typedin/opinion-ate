04101f1187011c4277ecf7b274b7f78c0c6c0927
AntoineC <antoine@a-f.studio>
2020-09-21 23:58:18 +0200

    tdd: can get a dish and its relationships

--- tests/Feature/Relationships/DishesTest.php
+++ tests/Feature/Relationships/DishesTest.php
@@ -2,10 +2,72 @@
 
 namespace Tests\Feature\Relationships;
 
+use App\Models\Comment;
+use App\Models\Dish;
+use App\Models\Rating;
+use App\Models\Restaurant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tests\Traits\CanDecodeJson;
+use Tests\Traits\HasHeader;
 
 class DishesTest extends TestCase
 {
+    use RefreshDatabase, HasHeader, CanDecodeJson;
+
+    /**
+     * @test
+     */
+    public function a_visitor_can_show_a_dish_with_its_relationships()
+    {
+        Dish::factory()
+            ->for(Restaurant::factory())
+            ->has(Comment::factory())
+            ->has(Rating::factory()->count(10)->state(["value" => 5.00]))
+            ->create([
+                "id" => 1,
+                "name" => "Jarret au Munster",
+            ]);
+
+        $response = $this->getJson(
+            self::API_URL . "/dishes/1",
+            $this->headersWithNoCredentials()
+        );
+
+        $response->assertStatus(200);
+
+        $this->assertJsonValueEquals(
+            $response->getContent(),
+            "$.data.id",
+            "1"
+        );
+
+        $this->assertJsonValueEquals(
+            $response->getContent(),
+            "$.data.attributes.name",
+            "Jarret au Munster"
+        );
+
+        $this->assertJsonValueEquals(
+            $response->getContent(),
+            "$.data.attributes.rating",
+            "5.0"
+        );
+
+        $this->assertArrayHasKey(
+            "relationships",
+            $this->decodedJson($response, "data")
+        );
+
+        $this->assertArrayHasKey(
+            "restaurant",
+            $this->decodedJson($response, "data.relationships")
+        );
+
+        $this->assertArrayHasKey(
+            "comments",
+            $this->decodedJson($response, "data.relationships")
+        );
+    }
 }
